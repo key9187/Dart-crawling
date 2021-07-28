420dfba3b22ec4f5f90b02bb1f117bdc14b7dc84
key9187 <44668992+key9187@users.noreply.github.com>
2021-07-28 16:33:35 +0900

    Update 12. 특수관계자 크롤링_20191011.R

--- 12. 특수관계자 크롤링_20191011.R
+++ 12. 특수관계자 크롤링_20191011.R
@@ -7,7 +7,6 @@ if(!require(zoo)){install.packages('zoo')}; library(zoo)
 if(!require(xlsx)){install.package('xlsx')}; library(xlsx)
 if(!require(data.table)){install.package('data.table')}; library(data.table)
 
-
 kospi200 <- read.csv(file='KOSPI200_web.csv')
 
 
@@ -15,7 +14,6 @@ kospi200 <- read.csv(file='KOSPI200_web.csv')
 
 ### 파이썬에서 크롤링한 결과불러오는 함수 정의 
 python_result_read <- function(number, start, end){
-  # file_link <- paste0('sample', number, '.xlsx')
   file_link <- paste0('sample', number, '.xlsx')
   result_dat = list()
   for(ind in (start-1):(end-1)){
@@ -24,9 +22,6 @@ python_result_read <- function(number, start, end){
     colnames(tmp_dat) <- as.character(tmp_dat[1,])
     result_dat[[(ind-start+2)]] <- tmp_dat[-1,]
   }
-  
-  
-  
   return(result_dat)
 }
 
